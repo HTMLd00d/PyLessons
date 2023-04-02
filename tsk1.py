e1516b1cc8d7af7ed1e71dc15cfc9927aa64e051
d00dez <statik19@yandex.ru>
2023-04-02 19:27:36 +0500

    task1

--- tsk1.py
+++ tsk1.py
@@ -1,1 +1,13 @@
-a = input("commit test111")
+n = int(input())
+count_tail = 0
+count_head = 0
+for i in range(n):
+    x = int(input())
+    if x == 0:
+        count_tail += 1
+    else:
+        count_head += 1
+if count_head > count_tail:
+    print(count_tail)
+else:
+    print(count_head)
